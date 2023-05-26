490ea65ff06f52908414cd79f19805eeb59c107e
AmodSeth <86409774+AmodSeth@users.noreply.github.com>
2023-05-26 20:06:07 +0530

    Time: 57 ms (64.07%) | Memory: 16.7 MB (54.21%) - LeetSync

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,19 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)): 
-            return False
-        countS={}
-        countT={}
+        return Counter(s) == Counter(t)
 
-        for i in range(len(t)):
-            #get is used to ignore key value error
-          countS[s[i]] = 1 + countS.get(s[i],0)  
-          countT[t[i]] = 1 + countT.get(t[i],0) 
 
-        for i in countS:
-            if countS[i] != countT.get(i,0):
-                return False
-        return True 
+        # if (len(s) != len(t)): 
+        #     return False
+        # countS={}
+        # countT={}
+
+        # for i in range(len(t)):
+        #     #get is used to ignore key value error
+        #   countS[s[i]] = 1 + countS.get(s[i],0)  
+        #   countT[t[i]] = 1 + countT.get(t[i],0) 
+
+        # for i in countS:
+        #     if countS[i] != countT.get(i,0):
+        #         return False
+        # return True 
 
         
 
